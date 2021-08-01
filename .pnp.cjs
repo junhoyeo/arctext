86ccb4d82a4ab06767eb62c7db3e91547b4cbbbb
Junho Yeo <hanaro0704@gmail.com>
2021-08-02 01:11:19 +0900

    chore: setup TypeScript SDK

--- .pnp.cjs
+++ .pnp.cjs
@@ -40,6 +40,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         [null, {
           "packageLocation": "./",
           "packageDependencies": [
+            ["typescript", "patch:typescript@npm%3A4.3.5#~builtin<compat/typescript>::version=4.3.5&hash=d8b4e7"]
           ],
           "linkType": "SOFT",
         }]
@@ -102,7 +103,8 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         ["workspace:.", {
           "packageLocation": "./",
           "packageDependencies": [
-            ["arctext", "workspace:."]
+            ["arctext", "workspace:."],
+            ["typescript", "patch:typescript@npm%3A4.3.5#~builtin<compat/typescript>::version=4.3.5&hash=d8b4e7"]
           ],
           "linkType": "SOFT",
         }]
